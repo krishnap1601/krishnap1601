0462c6f77ce31bf0313002eddc46553e964a0816
krishnap1601 <krishnaprasad8708@gmail.com>
2025-09-18 14:08:37 +0530

    Add files via upload

---
+++ c practicals/string2 6.3/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[100], str2[100];
+
+    printf("enter first string: ");
+    gets(str1);
+
+    printf("enter second string: ");
+    gets(str2);
+
+    printf("length of first string = %lu\n", strlen(str1));
+    printf("length of first string = %lu\n", strlen(str2));
+
+    if (strcmp(str1, str2) == 0)
+        printf("both string are equal.");
+    else
+        printf("string are not equal.");
+
+    return 0;
+}
